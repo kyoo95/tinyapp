cd0138407f449e5bc639c053309ab51907a2511e
Kevin <kevinyoo0203@yahoo.com>
2019-08-05 19:26:53 +0000

    added a second route and template

--- express_server.js
+++ express_server.js
@@ -8,6 +8,16 @@ const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+app.get("/urls", (req, res) => {
+  let templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+}); 
+
+app.get("/urls/:shortURL", (req, res) => {
+  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  res.render("urls_show", templateVars);
+});
+
 
 app.get("/", (req, res) => {
   res.send("Hello!");
