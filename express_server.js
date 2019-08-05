39abd3db566740ece7ec27299845cc2ebb3dbbf6
Kevin <kevinyoo0203@yahoo.com>
2019-08-05 20:30:58 +0000

    added string randomizer

--- express_server.js
+++ express_server.js
@@ -1,10 +1,25 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
 
+app.use(bodyParser.urlencoded({extended: true}));
+
+function generateRandomString() {
+  let randomChar = '';
+  let characters = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+  let charLength = 6;
+  for (let x = 0; x < charLength; x++) {
+    let rand = Math.floor(Math.random() * characters.length);
+    randomChar += characters.substring(rand, rand + 1);
+  }
+  return randomChar;
+};
+
 const urlDatabase = {
+
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
@@ -12,6 +27,10 @@ app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 }); 
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
 
 app.get("/urls/:shortURL", (req, res) => {
   let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
@@ -29,6 +48,11 @@ app.get("/urls.json", (req, res) => {
 
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
 
 app.listen(PORT, () => {
